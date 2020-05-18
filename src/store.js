4207d9c7bae78c088cd7e848e9534629d599f96a
Taesun Lee <superbsun@gmail.com>
2020-05-18 23:03:06 +0900

    upgrade using redux with react

---
+++ src/store.js
@@ -0,0 +1,40 @@
+import { createStore } from "redux";
+
+const ADD = "ADD";
+const DELETE = "DELETE";
+
+const addToDo = (text) => {
+  return {
+    type: ADD,
+    text,
+  };
+};
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE,
+    id: parseInt(id),
+  };
+};
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
+    case DELETE:
+      const cleaned = state.filter((toDo) => toDo.id !== action.id);
+      return cleaned;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+export const actionCreators = {
+  addToDo,
+  deleteToDo,
+};
+
+export default store;
